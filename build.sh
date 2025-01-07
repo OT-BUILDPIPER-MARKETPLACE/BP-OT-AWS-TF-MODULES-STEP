7dd3a7a97441a2be60bfb7b2ac7f1ab8a7a0563e
Aakash Tripathi <aakash.tripathi@opstree.com>
2025-01-07 13:37:13 +0530

    Adding credential and assume role capabilaties

--- build.sh
+++ build.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
+
 source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
+source /opt/buildpiper/shell-funtions/aws-functions.sh
 
-logInfoMessage "Creating for $MODULE"
-logInfoMessage "I'll create/update [$MODULE] whose properties are available at [$WORKSPACE] and have mounted at [$CODEBASE_DIR]"
-sleep  "$SLEEP_DURATION"
 
-cd  "$WORKSPACE"/"${CODEBASE_DIR}"
-cp /opt/buildpiper/modules/${MODULE}/*.tf .
+tfCodeLocation="${WORKSPACE}"/"${CODEBASE_DIR}"/"${TF_CODE_LOCATION}"
+logInfoMessage "I'll create/update terraform code  available at [$tfCodeLocation]"
+
+cd  "${tfCodeLocation}"
+
 
 logInfoMessage "Running below tf command"
 logInfoMessage "terraform $INSTRUCTION"
 
-terraform init
 
+if [[ -n "$TF_MODULE_GIT_HOST_NAME" && -n "$TF_MODULE_GIT_USER" && -n "$TF_MODULE_GIT_PAT" ]]; then
+  # Generate the ~/.netrc file
+  cat <<EOF > ~/.netrc
+machine $TF_MODULE_GIT_HOST_NAME
+    login $TF_MODULE_GIT_USER
+    password $TF_MODULE_GIT_PAT
+EOF
+
+  # Set secure permissions
+  chmod 600 ~/.netrc
+  logInfoMessage "Generated ~/.netrc with credentials for $TF_MODULE_GIT_HOST_NAME."
+else
+  logInfoMessage "No GIT Credentials Provided. ~/.netrc will not be created."
+fi
+
+
+if [[ -n "$ROLE_ARN" ]]; then
+  # Calling assumeRole with ROLE_ARN and SESSION_NAME
+  assumeRole "$ROLE_ARN" "$SESSION_NAME"
+else
+  logInfoMessage "ROLE_ARN is empty or not set.Using BP Role"
+fi
+
+
+terraform init
 case "$INSTRUCTION" in
 
   plan)
+    terraform init
     terraform plan -var-file="terraform.tfvars"
     ;;
 
